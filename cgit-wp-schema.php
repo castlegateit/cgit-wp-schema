807345fa3a1b12b60dfaba69c46de592e5015c6c
John Hughes <john@castlegateit.co.uk>
2018-01-26 14:09:04 +0000

    v0.2

--- cgit-wp-schema.php
+++ cgit-wp-schema.php
@@ -5,7 +5,7 @@
 Plugin Name: Castlegate IT WP Schema
 Plugin URI: https://github.com/castlegateit/cgit-wp-schema
 Description: Basic schema.org integration
-Version: 0.1
+Version: 0.2
 Author: Castlegate IT
 Author URI: https://www.castlegateit.co.uk/
 

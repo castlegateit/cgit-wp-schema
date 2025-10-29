6d56e273c38253b81fcabe18f7599380c7503b07
John Hughes <john.noreply@castlegateit.co.uk>
2025-10-29 09:34:16 +0000

    Increment version number

--- cgit-wp-schema.php
+++ cgit-wp-schema.php
@@ -4,7 +4,7 @@
  * Plugin Name:  Castlegate IT WP Schema
  * Plugin URI:   https://github.com/castlegateit/cgit-wp-schema
  * Description:  Basic schema.org integration.
- * Version:      1.2.1
+ * Version:      1.2.2
  * Requires PHP: 8.2
  * Author:       Castlegate IT
  * Author URI:   https://www.castlegateit.co.uk/
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
     wp_die('Access denied');
 }
 
-define('CGIT_WP_SCHEMA_VERSION', '1.2.0');
+define('CGIT_WP_SCHEMA_VERSION', '1.2.2');
 define('CGIT_WP_SCHEMA_PLUGIN_FILE', __FILE__);
 define('CGIT_WP_SCHEMA_PLUGIN_DIR', __DIR__);
 
